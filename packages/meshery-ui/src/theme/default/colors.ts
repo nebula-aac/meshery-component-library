f6ff2d338de84fa291732e59a85ae9ac20824075
Antonette Caldwell <acald.nebula@gmail.com>
2023-06-15 11:21:49 -0500

    init

---
+++ packages/meshery-ui/src/theme/default/colors.ts
@@ -0,0 +1,11 @@
+import { PaletteMode } from '@mui/material'
+
+export const light = {
+    mode: 'light' as PaletteMode,
+}
+
+export const dark = {
+    mode: 'dark' as PaletteMode,
+}
+
+export default { dark, light }
